ffeb7597d43c24c442c5025b62448b9408d2f0b5
Chris Weldon <chris@chrisweldon.net>
2011-10-05 22:59:25 -0500

    Creating the new solution and adding 2 basic unit tests to illustrate the basic capabilities of Moles.

---
+++ Tests/BasicMolesTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Moles.Framework;
+using Microsoft.Moles.Framework.Moles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: MoledType(typeof(System.DateTime))]
+
+namespace TestingInSP2010
+{
+    /// <summary>
+    /// These are basic tests that present a basis for how Moles works.
+    /// </summary>
+    [TestClass]
+    public class BasicMolesTests
+    {
+        /// <summary>
+        /// This is the most basic of tests to validate that Moles can detour the runtime
+        /// behavior of static methods. 
+        /// </summary>
+        [TestMethod]
+        [HostType("Moles")]
+        public void TestDateTimeDetour()
+        {
+            System.Moles.MDateTime.NowGet = () => new DateTime(2011, 10, 1, 0, 0, 0);
+            Assert.AreEqual(new DateTime(2011, 10, 1, 0, 0, 0), DateTime.Now, "Dates do not match.");
+        }
+
+        /// <summary>
+        /// Tests that no matter what date relation we compare to, with Moles, I can ensure
+        /// that operations on methods are detoured. Here, a difference between any two 
+        /// arbitrary dates should result in a single day difference.
+        /// </summary>
+        [TestMethod, HostType("Moles")]
+        public void ExtendedDateTimeTest()
+        {
+            DateTime now = DateTime.Now;
+            DateTime backToTheFuture = new DateTime(2015, 10, 21);
+            System.Moles.MDateTime.SubtractionOpDateTimeDateTime =
+                (dateLeft, dateRight) => new TimeSpan(1, 0, 0, 0);
+            Assert.AreEqual(new TimeSpan(1, 0, 0, 0), backToTheFuture - now);
+        }
+    }
+}

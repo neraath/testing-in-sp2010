edd490be3d0d77e8594be7e1182328c602e4b48b
Chris Weldon <chris@chrisweldon.net>
2011-10-06 20:00:37 -0500

    First set of new source and tests for SharePoint Moles Examples.

---
+++ SharePoint.Library.Tests/WebSiteManagerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SharePoint.Moles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SharePoint.Library.Tests
+{
+    [TestClass]
+    public class WebSiteManagerTests
+    {
+        private WebSiteManager manager;
+
+        /// <summary>
+        /// Tests the standard use case for the web title. 
+        /// </summary>
+        /// <remarks>
+        /// In this first test case, moles only has to mock a few items. We
+        /// use BehaveAsNotImplemented() to help us identify what we miss. 
+        /// </remarks>
+        [TestMethod, HostType("Moles")]
+        public void GetSiteNameReturnsWebTitle()
+        {
+            // Arrange.
+            string expectedTitle = "Test Site";
+            MSPSite.BehaveAsNotImplemented();
+            MSPWeb.BehaveAsNotImplemented();
+            MSPSite.ConstructorString = (instance, url) => { };
+            MSPSite.AllInstances.Dispose = (instance) => { };
+            MSPSite.AllInstances.OpenWeb = (instance) =>
+                                               {
+                                                   MSPWeb web = new MSPWeb();
+                                                   web.Dispose = () => { };
+                                                   web.TitleGet = () => expectedTitle;
+                                                   return web;
+                                               };
+
+            // Act.
+            WebSiteManager manager = new WebSiteManager("http://test");
+            string siteName = manager.GetSiteName();
+
+            // Assert.
+            Assert.AreEqual(expectedTitle, siteName, "Site Name does not match expected title.");
+        }
+    }
+}
---
+++ SharePoint.Library/WebSiteManager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SharePoint;
+
+namespace SharePoint.Library
+{
+    public class WebSiteManager
+    {
+        private string siteUrl;
+
+        public WebSiteManager(string url)
+        {
+            this.siteUrl = url;
+        }
+
+        public string GetSiteName()
+        {
+            using (SPSite site = new SPSite(siteUrl))
+            {
+                using (SPWeb web = site.OpenWeb())
+                {
+                    return web.Title;
+                }
+            }
+        }
+    }
+}
